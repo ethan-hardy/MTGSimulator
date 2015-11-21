8538782a2a5482bace85070c2fb0ec4c63f6876e
Ethan Hardy <ehardy45@gmail.com>
2015-11-21 18:16:26 -0500

    Initial skeleton of what I need; nothing does anything yet.

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+'use strict';
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+module.exports = {
+    entry: './public_html/script/src/index.es6',
+    output: {
+        path: './public_html/',
+        filename: 'script/mtg.js'
+    },
+    module: {
+        loaders: [
+            { test: /\.scss$/, loader: ExtractTextPlugin.extract('style', 'css!autoprefixer!sass') },
+            { test: /\.es6$/, loader: 'babel' }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('style/main.css')
+    ]
+};
