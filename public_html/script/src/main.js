489c69d9fa10014414e9a5d9a4fd8114537ac0e7
Ethan Hardy <ehardy45@gmail.com>
2015-11-29 22:44:09 -0500

    Made the basic layout. Still need to come up with a better aesthetic but it's fine for now.

---
+++ public_html/script/src/main.js
@@ -0,0 +1,9 @@
+(function() {
+  /*$(".hand").each(function(obj) {
+
+  });
+  $(".singleCardZone").each(function() {
+      this.height(40);
+  });*.*/
+  $("#battlefield").height(window.innerHeight - $(".playerSection").outerHeight(true) * 2 - 20);
+})()
